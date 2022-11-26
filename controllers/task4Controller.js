4b3a9318472e98ce540b472060e7418d4cdaae25
jacknguyen <nguyenphucbao098@gmail.com>
2022-11-26 23:17:31 +0700

    fix: add name task 4

--- controllers/task4Controller.js
+++ controllers/task4Controller.js
@@ -1,15 +1,18 @@
-const { zodiacs } = require("../models/data");
+const { zodiacs } = require('../models/data');
+const author = '20127118 - Nguyễn Phúc Bảo';
 
 const renderList = (req, res) => {
-  res.render("task4", {
+  res.render('task4', {
     zodiacs,
+    author,
   });
 };
 const renderDetails = (req, res) => {
-  res.render("task4-details", {
+  res.render('task4-details', {
     zodiac: zodiacs.find(
       (item) => item.name.toLowerCase() == req.params.name.toLowerCase()
     ),
+    author,
   });
 };
 module.exports = {
