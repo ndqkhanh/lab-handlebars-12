ffec99f71d82eefc4547ae38bb0d40345f61484a
Nguyễn Đinh Quang Khánh <20127530@student.hcmus.edu.vn>
2022-11-26 23:10:33 +0700

    refactor: fix let to const

--- controllers/task2Controller.js
+++ controllers/task2Controller.js
@@ -3,9 +3,9 @@ const title = "Jars Saving";
 
 const showDefault = (req, res) => {
   const salary = Number(req.query.salary) || 0;
-  let jar55 = (salary * 0.55).toFixed(2);
-  let jar10 = (salary * 0.1).toFixed(2);
-  let jar5 = (salary * 0.05).toFixed(2);
+  const jar55 = (salary * 0.55).toFixed(2);
+  const jar10 = (salary * 0.1).toFixed(2);
+  const jar5 = (salary * 0.05).toFixed(2);
   res.render("task2", { author, title, jar55, jar10, jar5 });
 };
 
