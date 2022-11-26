1df65e28cdbcdad17ee4454369020add9b314fbd
simplord542 <81230977+simplord542@users.noreply.github.com>
2022-11-26 13:04:31 +0700

    set up

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# lab-handlebars-12
+# lab-handlebars
+
+12
+
+1. 20127027 - Nguyễn Thanh Hoàng - BaiTap 1
---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+
+const expressHbs = require("express-handlebars");
+
+app.engine(
+  "hbs",
+  expressHbs.engine({
+    layoutDir: __dirname + "/views/layouts",
+    partialsDir: __dirname + "/views/partials",
+    defaultLayout: "layout",
+    extname: "hbs",
+  })
+);
+app.set("view engine", "hbs");
+
+app.use(express.static(__dirname + "/Handlebars-StaticFiles"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/index", (req, res) => {
+  res.render("index");
+});
+app.get("/task1", require('./routes/task1Route'));
+app.get("/task2", (req, res) => {
+  res.render("task2");
+});
+app.get("/task3", (req, res) => {
+  res.render("task3");
+});
+app.get("/task4", (req, res) => {
+  res.render("task4");
+});
+app.set("port", process.env.PORT || 3000);
+app.listen(app.get("port"), () => {
+  console.log("c2c");
+});
