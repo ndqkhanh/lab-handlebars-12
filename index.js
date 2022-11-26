fe7faf03ebd4242820aac7ba4de258ddf33e31ef
Nguyễn Đinh Quang Khánh <20127530@student.hcmus.edu.vn>
2022-11-26 22:03:25 +0700

    build: task 2 first commit

--- index.js
+++ index.js
@@ -19,19 +19,25 @@ app.use(express.static(__dirname + "/Handlebars-StaticFiles"));
 app.get("/", (req, res) => {
   res.render("index");
 });
+
 app.get("/index", (req, res) => {
   res.render("index");
 });
+
 app.get("/task1", require("./routes/task1Route"));
+
 app.get("/task2", (req, res) => {
   res.render("task2");
 });
+
 app.get("/task3", (req, res) => {
   res.render("task3");
 });
+
 app.get("/task4", (req, res) => {
   res.render("task4");
 });
+
 app.set("port", process.env.PORT || 3000);
 app.listen(app.get("port"), () => {
   console.log("Server is running on port " + app.get("port"));
---
+++ routes/task2Route.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const route = express.Router();
+const controller = require("../controllers/task2Controller");
+
+module.exports = route;
