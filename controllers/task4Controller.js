7604f263599a95d70e2055c741ee70279076ca49
jacknguyen <nguyenphucbao098@gmail.com>
2022-11-26 21:42:57 +0700

    add task 4

---
+++ controllers/task4Controller.js
@@ -0,0 +1,18 @@
+const { zodiacs } = require('../models/data');
+
+const renderList = (req, res) => {
+  res.render('task4', {
+    zodiacs,
+  });
+};
+const renderDetails = (req, res) => {
+  res.render('task4-details', {
+    zodiac: zodiacs.find(
+      (item) => item.name.toLowerCase() == req.params.name.toLowerCase()
+    ),
+  });
+};
+module.exports = {
+  renderList,
+  renderDetails,
+};
--- index.js
+++ index.js
@@ -1,38 +1,36 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-
-const expressHbs = require("express-handlebars");
+const expressHbs = require('express-handlebars');
 
 app.engine(
-  "hbs",
+  'hbs',
   expressHbs.engine({
-    layoutDir: __dirname + "/views/layouts",
-    partialsDir: __dirname + "/views/partials",
-    defaultLayout: "layout",
-    extname: "hbs",
+    layoutDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials',
+    defaultLayout: 'layout',
+    extname: 'hbs',
   })
 );
-app.set("view engine", "hbs");
+app.set('view engine', 'hbs');
 
-app.use(express.static(__dirname + "/Handlebars-StaticFiles"));
+app.use(express.static(__dirname + '/Handlebars-StaticFiles'));
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.get('/', (req, res) => {
+  res.render('index');
 });
-app.get("/index", (req, res) => {
-  res.render("index");
+app.get('/index', (req, res) => {
+  res.render('index');
 });
-app.get("/task1", require("./routes/task1Route"));
-app.get("/task2", (req, res) => {
-  res.render("task2");
+app.get('/task1', require('./routes/task1Route'));
+app.get('/task2', (req, res) => {
+  res.render('task2');
 });
-app.get("/task3", (req, res) => {
-  res.render("task3");
+app.get('/task3', (req, res) => {
+  res.render('task3');
 });
-app.get("/task4", (req, res) => {
-  res.render("task4");
-});
-app.set("port", process.env.PORT || 3000);
-app.listen(app.get("port"), () => {
-  console.log("c2c");
+app.use('/task4', require('./routes/task4Route'));
+
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'), () => {
+  console.log('http://localhost:3000');
 });
